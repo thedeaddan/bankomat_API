5d1849391db9547a9959f5a9ec05c79a02f1a08b
Даниил <40400854+thedeaddan@users.noreply.github.com>
2023-06-30 12:28:41 +0300

    Переработан полностью

--- site_.py
+++ site_.py
@@ -1,44 +1,160 @@
 from flask import Flask, jsonify, request
 import sqlite3
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 db_name = "atm_system.db"
+
+import logging
+
+# Создаем объект логгера
+logger = logging.getLogger(__name__)
+
+# Устанавливаем уровень логгирования (можно настроить)
+logger.setLevel(logging.ERROR)
+
+# Создаем объект обработчика, который записывает логи в файл
+handler = logging.FileHandler('errors.log')
+
+# Устанавливаем уровень логгирования обработчика
+handler.setLevel(logging.ERROR)
+
+# Создаем форматтер для логгирования
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Устанавливаем форматтер для обработчика
+handler.setFormatter(formatter)
+
+# Добавляем обработчик в логгер
+logger.addHandler(handler)
+
+
+import random
+import string
+
+def generate_account_number():
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    while True:
+        # Generate a random account number
+        account_number = ''.join(random.choices(string.digits, k=10))
+
+        # Check if the account number already exists in the database
+        cursor.execute("SELECT id FROM users WHERE account_number=?", (account_number,))
+        result = cursor.fetchone()
+        if result is None:
+            # Account number is unique
+            break
+
+    conn.close()
+    return account_number
+@app.route("/transfer", methods=["POST"])
+def money_send():
+    operation_data = request.json
+    account_number= operation_data["sender"]
+    reciever=operation_data["reciever"]
+    try:
+        value=int(operation_data["value"])
+    except:
+         return jsonify({"message": "Сумма перевода не является числом"})
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT balance FROM users WHERE account_number = ?",(account_number,))
+    bal = cursor.fetchone()
+    try:
+        balace_sender = int(bal[0])
+        if value > balace_sender:
+            create_operation(account_number, "Транзакция", f"Списание на сумму {value} отклонено. Причина: Недостаточно средств.")
+            return jsonify({"message":"На счёте отправителя недостаточно средств."})
+        elif value < 0:
+            return jsonify({"message":"Сумма не может быть меньше нуля."})
+        else:
+            cursor.execute("SELECT balance FROM users WHERE account_number = ?",(reciever,))
+            rec_bal = cursor.fetchone()
+            try:
+                rec_bal = int(rec_bal[0])
+                balace_sender = balace_sender - value
+                cursor.execute("UPDATE users SET balance=? WHERE account_number =? ",(balace_sender,account_number,))
+                rec_bal = rec_bal + value
+                cursor.execute("UPDATE users SET balance=? WHERE account_number =? ",(rec_bal,reciever,))
+                conn.commit()
+                conn.close()
+                create_operation(account_number, "Транзакция", f"Списание средств {value}р. На счет {reciever}")
+                create_operation(reciever, "Транзакция", f"Пополнение счёта на {value}р. От {account_number}")
+                return jsonify({"message":f"Успешный перевод на сумму {value} с счёта {account_number} на счёт {reciever}"})
+            except Exception as e:
+                return jsonify({"message":"Пользователя которому вы отправляете деньги не существует. Или ошибка "+str(e)})
+    except:
+        return jsonify({"message": "Недостаточно средств или пользователь не существует."})
+
+@app.route("/add_money", methods=["POST"])
+def add_money():
+    operation_data = request.json
+    account_number = operation_data["account_number"]
+    try:
+        value=int(operation_data["value"])
+        if value < 0:
+            return jsonify({"message":"Сумма не может быть меньше нуля."})
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+        cursor.execute("SELECT balance FROM users WHERE account_number = ?",(account_number,))
+        balance = cursor.fetchone()[0]
+        try:
+            balance = int(balance)+value
+            cursor.execute("UPDATE users SET balance=? WHERE account_number =? ",(balance,account_number,))
+            conn.commit()
+            conn.close()
+            create_operation(account_number, "Транзакция", f"Пополнение счёта на {value} р.")
+            return jsonify({"message": f"Успешное пополнение счёта {account_number} на {value}р. "})
+        except Exception as e:
+            return jsonify({"message": "Ошибка пополнения "+e})
+    except Exception as e:
+        return jsonify({"message": "Сумма перевода не является числом"+str(e)})
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 ###ОПЕРАЦИИ###
 # Получить список всех операций пользователя
 @app.route("/users/<int:user_id>/operations", methods=["GET"])
 def get_operations(user_id):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM operations WHERE user_id=?", (user_id,))
-    operations = cursor.fetchall()
-    conn.close()
-    return jsonify(operations)
+    try:
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+        if user_id == 0:
+            cursor.execute("SELECT * FROM operations")
+        else:
+            cursor.execute("SELECT * FROM operations WHERE id=?", (user_id,))
+        operations = cursor.fetchall()
+        conn.close()
+        return jsonify(operations)
+    except Exception as e:
+        logger.error('Error occurred: %s', str(e))
 
-# Создать новую операцию
-@app.route("/users/<int:user_id>/operations", methods=["POST"])
-def create_operation(user_id):
-    operation_data = request.json
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO operations (user_id, type, amount, currency, date) VALUES (?, ?, ?, ?, ?)",
-                   (user_id, operation_data["type"], operation_data["amount"], operation_data["currency"], operation_data["date"]))
-    operation_id = cursor.lastrowid
-    conn.commit()
-    conn.close()
-    return jsonify({"id": operation_id}), 201
 
-# Обновить информацию об операции
-@app.route("/users/<int:user_id>/operations/<int:operation_id>", methods=["PUT"])
-def update_operation(user_id, operation_id):
-    operation_data = request.json
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    cursor.execute("UPDATE operations SET type=?, amount=?, currency=?, date=? WHERE id=? AND user_id=?",
-                   (operation_data["type"], operation_data["amount"], operation_data["currency"], operation_data["date"], operation_id, user_id))
-    conn.commit()
-    conn.close()
-    return jsonify({"message": "Operation updated successfully"})
+def create_operation(user_id, type_, description):
+    try:
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+
+        # Generate the current date and time
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        cursor.execute("INSERT INTO operations (user_id, type, description, date) VALUES (?, ?, ?, ?)",
+                       (user_id, type_, description, date))
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        logger.error('Error occurred: %s', str(e))
 
 # Удалить операцию
 @app.route("/users/<int:user_id>/operations/<int:operation_id>", methods=["DELETE"])
@@ -159,22 +275,28 @@ def get_users():
 # Обработчик запроса на создание нового пользователя
 @app.route("/users", methods=["POST"])
 def create_user():
+    print(request)
     user_data = request.json
+    print(request.json)
     conn = sqlite3.connect(db_name)
+    account_number = generate_account_number()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (first_name, last_name, pin, balance) VALUES (?, ?, ?, ?)",
-                   (user_data["first_name"], user_data["last_name"], user_data["pin"], user_data["balance"]))
+    cursor.execute("INSERT INTO users (account_number, first_name, last_name, pin, balance) VALUES (?, ?, ?, ?, ?)",
+                   (account_number,user_data["first_name"], user_data["last_name"], user_data["pin"], user_data["balance"]))
     conn.commit()
     new_user_id = cursor.lastrowid
+    cursor.execute("SELECT account_number FROM users WHERE id=?", (new_user_id,))
+    new_user_ab = cursor.fetchone()[0]
+    create_operation(new_user_ab, "Системное сообщение", f"Пользователь создан.")
     conn.close()
-    return jsonify({"id": new_user_id})
+    return jsonify({"id": new_user_ab})
 
 # Обработчик запроса на получение информации о конкретном пользователе
 @app.route("/users/<int:user_id>", methods=["GET"])
 def get_user(user_id):
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
+    cursor.execute("SELECT * FROM users WHERE account_number=?", (user_id,))
     user = cursor.fetchone()
     conn.close()
     return jsonify(user)
@@ -197,9 +319,19 @@ def update_user(user_id):
 def delete_user(user_id):
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM users WHERE id=?", (user_id,))
+
+    # Проверка наличия пользователя в базе
+    cursor.execute("SELECT id FROM users WHERE account_number=?", (user_id,))
+    user = cursor.fetchone()
+    if not user:
+        conn.close()
+        return "Пользователя нет", 404
+
+    # Удаление пользователя из базы
+    cursor.execute("DELETE FROM users WHERE account_number=?", (user_id,))
     conn.commit()
     conn.close()
+    create_operation(user_id, "Системное сообщение", f"Пользователь удалён.")
     return "", 204
 
 ###БАНКОМАТЫ###
@@ -297,4 +429,4 @@ def delete_exchange_rate(exchange_rate_id):
 
 # Запуск приложения
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=5050)
